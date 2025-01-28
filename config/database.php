af8cdd998855ccdf80bed859841ce59f8334f990
adea-123 <as70563@ubt-uni.net>
2025-01-29 00:38:28 +0100

    krijimi i database.php file

---
+++ config/database.php
@@ -0,0 +1,18 @@
+<?php
+
+function getDatabaseConnection() {
+    $host = 'localhost';
+    $dbname = 'omnifestivaldb';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+        exit();
+    }
+}
+?>
